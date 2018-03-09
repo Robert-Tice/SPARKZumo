f3e4149f9f5ad40be7addccc0675acdfc39f64b4
Robert Tice <tice@adacore.com>
2018-03-09 12:02:44 -0500

    Adding code to create the graph for the lookup table.

--- utils/geolookup.py
+++ utils/geolookup.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python2
+
 import textwrap
 
 from shapely.geometry import Point
 from shapely.geometry.polygon import LinearRing, Polygon
+
+import matplotlib.pyplot as plt
+from descartes.patch import PolygonPatch
 
 import sys
 
@@ -17,27 +22,45 @@ y_diff = yMax - int(yMax / 1.73)
 shapes = {
     "Online": {
         "points": [],
-        "polygon": Polygon([(xMax, yMax), (0, yMax), (0, 0), (xMax, y_diff)])
+        "polygon": Polygon([(xMax, yMax), (0, yMax), (0, 0), (xMax, y_diff)]),
+        "color": "#ff00ff",
+        "point": Point(xMax, yMax),
+        "txtpos": Point(xMax - 4.5, yMax + 0.5)
     },
     "BranchLeft": {
         "points": [],
-        "polygon": Polygon([(xMin, y_diff), (0, 0), (0, yMax), (xMin, yMax)])
+        "polygon": Polygon([(xMin, y_diff), (0, 0), (0, yMax), (xMin, yMax)]),
+        "color": "#0000ff",
+        "point": Point(xMin, yMax),
+        "txtpos": Point(xMin + 1, yMax + 0.5)
     },
     "Perp": {
         "points": [],
-        "polygon": Polygon([(xMin, -1 * y_diff), (0, 0), (xMin, y_diff)])
+        "polygon": Polygon([(xMin, -1 * y_diff), (0, 0), (xMin, y_diff)]),
+        "color": "#ff0000",
+        "point": Point(xMin, 0),
+        "txtpos": Point(xMin + 1, 0)
     },
     "Lost": {
         "points": [],
-        "polygon": Polygon([(0, yMin), (0, 0), (xMin, -1 * y_diff), (xMin, yMin)])
+        "polygon": Polygon([(0, yMin), (0, 0), (xMin, -1 * y_diff), (xMin, yMin)]),
+        "color": "#00ff00",
+        "point": Point(xMin, yMin),
+        "txtpos": Point(xMin + 1, yMin - 1.5)
     },
     "Fork": {
         "points": [],
-        "polygon": Polygon([(xMax, -1 * y_diff), (0, 0), (0, yMin), (xMax, yMin)])
+        "polygon": Polygon([(xMax, -1 * y_diff), (0, 0), (0, yMin), (xMax, yMin)]),
+        "color": "#ffaa00",
+        "point": Point(xMax, yMin),
+        "txtpos": Point(xMax - 3, yMin - 1.5)
     },
     "BranchRight": {
         "points": [],
-        "polygon": Polygon([(xMax, y_diff), (0, 0), (xMax, -1 * y_diff)])
+        "polygon": Polygon([(xMax, y_diff), (0, 0), (xMax, -1 * y_diff)]),
+        "color": "#633c01",
+        "point": Point(xMax, 0),
+        "txtpos": Point(xMax + 1, 0)
     }
 }
 
@@ -94,6 +117,29 @@ def GenerateOutputFile():
     with open("output.txt", "w") as file:
         file.write(output_str)
 
+def GeneratePlots():
+    plt.figure()
+    ax = plt.axes()
+    ax.set_aspect('equal')
+
+    for key, shape in shapes.iteritems():
+        patch = PolygonPatch(shape["polygon"], facecolor=shape["color"], edgecolor=[0,0,0], label=key)
+        plt.plot(shape["point"].x, shape["point"].y, 'ko')
+        plt.text(shape["txtpos"].x, shape["txtpos"].y, key)
+
+        ax.add_patch(patch)
+
+    thresh_circle = plt.Circle((0, 0), 7, facecolor='#ffffff', edgecolor=[0,0,0])
+    ax.add_patch(thresh_circle)
+
+    plt.text(-2, -1, "Noise")
+
+    plt.xlim(xMin - 2, xMax + 2)
+    plt.ylim(yMin - 2, yMax + 2)
+    lgd = ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+    plt.savefig('graph.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+
 def testArray(x, y):
     populatePoints()
     array = synthesizeArray()
@@ -102,10 +148,10 @@ def testArray(x, y):
 
     return array[x][y]
 
-
 def main():
     if len(sys.argv) == 1:
         GenerateOutputFile()
+        GeneratePlots()
     elif len(sys.argv) == 3:
         x = int(sys.argv[1])
         y = int(sys.argv[2])
